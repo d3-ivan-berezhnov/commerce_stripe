0f34009f44855a426638c7084ea6aaae7a3f45fe
Artusamak <Artusamak@309607.no-reply.drupal.org>
2015-08-26 10:57:46 -0700

    Issue #2545400 by Artusamak: Let modules alter the order description

--- commerce_stripe.api.php
+++ commerce_stripe.api.php
@@ -12,3 +12,15 @@ function hook_commerce_stripe_metadata($order) {
     'order_number' => $order->order_number,
   );
 }
+
+
+/**
+ * Alter the description of the order sent to Stripe in the payment details.
+ */
+function hook_commerce_stripe_order_description_alter(&$description, $order) {
+  // Example of alteration of the description.
+  if ($order->data['item_purchased'] == 'token_card') {
+    $card_id = rand(1, 10000);
+    $description = t('Token card id: %token_card_id', array('%token_card_id' => $card_id));
+  }
+}
