96c840bbc4200c7bd95786d809bf991e864342f4
torgospizza <torgospizza@151333.no-reply.drupal.org>
2015-11-11 17:19:02 -0800

    Issue #2545400 by Artusamak, torgosPizza: Let modules alter the charge data

--- commerce_stripe.api.php
+++ commerce_stripe.api.php
@@ -17,10 +17,12 @@ function hook_commerce_stripe_metadata($order) {
 /**
  * Alter the description of the order sent to Stripe in the payment details.
  */
-function hook_commerce_stripe_order_description_alter(&$description, $order) {
+function hook_commerce_stripe_order_charge_alter(&$charge, $order) {
   // Example of alteration of the description.
   if ($order->data['item_purchased'] == 'token_card') {
     $card_id = rand(1, 10000);
-    $description = t('Token card id: %token_card_id', array('%token_card_id' => $card_id));
+    $charge['description'] = t('Token card id: %token_card_id', array('%token_card_id' => $card_id));
   }
+  // Add application fees.
+  $charge['application_fee'] = rand(1, 1000);
 }
