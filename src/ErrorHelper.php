248a9bbe7bf4ad8a24425a15d7f0565c7dff2eb6
tavi toporjinschi <vasike@gmail.com>
2017-03-09 16:31:24 +0000

    Issue #2859278 by vasike: ErrorHelper implementation - First implementation.

--- src/ErrorHelper.php
+++ src/ErrorHelper.php
@@ -3,10 +3,10 @@
 namespace Drupal\commerce_stripe;
 
 use Drupal\commerce_payment\Exception\AuthenticationException;
+use Drupal\commerce_payment\Exception\DeclineException;
 use Drupal\commerce_payment\Exception\HardDeclineException;
 use Drupal\commerce_payment\Exception\InvalidRequestException;
 use Drupal\commerce_payment\Exception\InvalidResponseException;
-use Drupal\commerce_payment\Exception\SoftDeclineException;
 
 /**
  * Translates Stripe exceptions and errors into Commerce exceptions.
@@ -16,37 +16,44 @@ class ErrorHelper {
   /**
    * Translates Stripe exceptions into Commerce exceptions.
    *
-   * @param \Stripe\Exception $exception
+   * @param \Stripe\Error\Base $exception
    *   The Stripe exception.
    *
    * @throws \Drupal\commerce_payment\Exception\PaymentGatewayException
    *   The Commerce exception.
    */
-  public static function handleException(\Stripe\Exception $exception) {
-//    if ($exception instanceof \Stripe\Exception\Authentication) {
-//      throw new AuthenticationException('Stripe authentication failed.');
-//    }
-//    elseif ($exception instanceof \Stripe\Exception\Authorization) {
-//      throw new AuthenticationException('The used API key is not authorized to perform the attempted action.');
-//    }
-//    elseif ($exception instanceof \Stripe\Exception\NotFound) {
-//      throw new InvalidRequestException('Stripe resource not found.');
-//    }
-//    elseif ($exception instanceof \Stripe\Exception\UpgradeRequired) {
-//      throw new InvalidRequestException('The Stripe client library needs to be updated.');
-//    }
-//    elseif ($exception instanceof \Stripe\Exception\TooManyRequests) {
-//      throw new InvalidRequestException('Too many requests.');
-//    }
-//    elseif ($exception instanceof \Stripe\Exception\ServerError) {
-//      throw new InvalidResponseException('Server error.');
-//    }
-//    elseif ($exception instanceof \Stripe\Exception\DownForMaintenance) {
-//      throw new InvalidResponseException('Request timed out.');
-//    }
-//    else {
-//      throw new InvalidResponseException($exception->getMessage());
-//    }
+  public static function handleException(\Stripe\Error\Base $exception) {
+    if ($exception instanceof \Stripe\Error\Card) {
+      \Drupal::logger('commerce_stripe')->warning($exception->getMessage());
+      throw new DeclineException('We encountered an error processing your card details. Please verify your details and try again.');
+    }
+    elseif ($exception instanceof \Stripe\Error\RateLimit) {
+      \Drupal::logger('commerce_stripe')->warning($exception->getMessage());
+      throw new InvalidRequestException('Too many requests.');
+    }
+    elseif ($exception instanceof \Stripe\Error\InvalidRequest) {
+      \Drupal::logger('commerce_stripe')->warning($exception->getMessage());
+      throw new InvalidRequestException('Invalid parameters were supplied to Stripe\'s API.');
+    }
+    elseif ($exception instanceof \Stripe\Error\Authentication) {
+      \Drupal::logger('commerce_stripe')->warning($exception->getMessage());
+      throw new AuthenticationException('Stripe authentication failed.');
+    }
+    elseif ($exception instanceof \Stripe\Error\ApiConnection) {
+      \Drupal::logger('commerce_stripe')->warning($exception->getMessage());
+      throw new InvalidResponseException('Network communication with Stripe failed.');
+    }
+    elseif ($exception instanceof \Stripe\Error\ApiConnection) {
+      \Drupal::logger('commerce_stripe')->warning($exception->getMessage());
+      throw new InvalidResponseException('Network communication with Stripe failed.');
+    }
+    elseif ($exception instanceof \Stripe\Error\Base) {
+      \Drupal::logger('commerce_stripe')->warning($exception->getMessage());
+      throw new InvalidResponseException('There was an error with Stripe request.');
+    }
+    else {
+      throw new InvalidResponseException($exception->getMessage());
+    }
   }
 
   /**
@@ -59,68 +66,26 @@ class ErrorHelper {
    *   The Commerce exception.
    */
   public static function handleErrors($result) {
-//    if ($result['_values']['status'] == 'succeeded') {
-//      return;
-//    }
-//    $errors = $result->errors->deepAll();
-//    if (!empty($errors)) {
-//      // https://developers.stripepayments.com/reference/general/validation-errors/all/php
-//      // Validation errors can be due to a module error (mapped to
-//      // InvalidRequestException) or due to a user input error (mapped to
-//      // a HardDeclineException).
-//      $hard_decline_codes = [81813, 91828, 81736, 81737, 81750, 91568];
-//      foreach($errors AS $error) {
-//        if (in_array($error->code, $hard_decline_codes)) {
-//          throw new HardDeclineException($error->message, $error->code);
-//        }
-//        else {
-//          throw new InvalidRequestException($error->message, $error->code);
-//        }
-//      }
-//    }
-//
-//    // Both verification and the transaction can result in the same errors.
-//    $error_statuses = [
-//      'settlement_declined',
-//      'gateway_rejected',
-//      'processor_declined',
-//    ];
-//    if ($result->verification && in_array($result->verification->status, $error_statuses)) {
-//      $error = $result->verification;
-//      $status = $result->verification->status;
-//    }
-//    elseif ($result->transaction && in_array($result->verification->status, $error_statuses)) {
-//      $error = $result->verification;
-//      $status = $result->verification->status;
-//    }
-//
-//    if ($status == 'settlement_declined') {
-//      $code = $error->processorSettlementResponseCode;
-//      $text = $error->processorSettlementResponseText;
-//      throw new HardDeclineException($text, $code);
-//    }
-//    elseif ($status == 'gateway_rejected') {
-//      $reason = $error->gatewayRejectionReason;
-//      throw new HardDeclineException('Rejected by the gateway. Reason: ' . $reason);
-//    }
-//    elseif ($status == 'processor_declined') {
-//      // https://developers.stripepayments.com/reference/general/processor-responses/authorization-responses
-//      $soft_decline_codes = [
-//        2000, 2001, 2002, 2003, 2009, 2016, 2021, 2025, 2026, 2033, 2034, 2035,
-//        2038, 2040, 2042, 2046, 2048, 2050, 2054, 2057, 2062,
-//      ];
-//      $code = $error->processorResponseCode;
-//      $text = $error->processorResponseText;
-//      if (!empty($error->additionalProcessorResponse)) {
-//        $text .= ' (' . $error->additionalProcessorResponse . ')';
-//      }
-//      if (in_array($code, $soft_decline_codes) || ($code >= 2092 && $code <= 3000)) {
-//        throw new SoftDeclineException($text, $code);
-//      }
-//      else {
-//        throw new HardDeclineException($text, $code);
-//      }
-//    }
+    if ($result['_values']['status'] == 'succeeded') {
+      return;
+    }
+    $errors = $result->errors->deepAll();
+    if (!empty($errors)) {
+      // https://stripe.com/docs/api?lang=php#errors
+      // Validation errors can be due to a module error (mapped to
+      // InvalidRequestException) or due to a user input error (mapped to
+      // a HardDeclineException).
+      $hard_decline_codes = [500, 502, 503, 504];
+      foreach($errors AS $error) {
+        if (in_array($error->code, $hard_decline_codes)) {
+          throw new HardDeclineException($error->message, $error->code);
+        }
+        else {
+          throw new InvalidRequestException($error->message, $error->code);
+        }
+      }
+    }
+
   }
 
 }
