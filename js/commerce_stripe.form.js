96c2ffed10e2181e5073f5c115232f6fb420dd0a
tavi toporjinschi <vasike@gmail.com>
2017-03-13 10:10:47 +0000

    Clean-Up on js file.

--- js/commerce_stripe.form.js
+++ js/commerce_stripe.form.js
@@ -5,116 +5,116 @@
 
 (function ($, Drupal, drupalSettings, stripe) {
 
-    'use strict';
+  'use strict';
 
-    /**
-     * Attaches the commerceStripeForm behavior.
-     *
-     * @type {Drupal~behavior}
-     *
-     * @prop {Drupal~behaviorAttach} attach
-     *   Attaches the commerceStripeForm behavior.
-     *
-     * @see Drupal.commerceStripe
-     */
-    Drupal.behaviors.commerceStripeForm = {
-        attach: function (context) {
-            if (typeof drupalSettings.commerceStripe.fetched == 'undefined') {
-                drupalSettings.commerceStripe.fetched = true;
-                // Clear the token every time the payment form is loaded. We only need the token
-                // one time, as it is submitted to Stripe after a card is validated. If this
-                // form reloads it's due to an error; received tokens are stored in the checkout pane.
-                $('#stripe_token').val('');
-                Stripe.setPublishableKey(drupalSettings.commerceStripe.publishableKey);
-                var $form = $('.stripe-form', context).closest('form');
-                var stripeResponseHandler = function (status, response) {
-                    if (response.error) {
-                        // Show the errors on the form
-                        $form.find('.payment-errors').text(response.error.message);
-                        $form.find('button').prop('disabled', false);
-                    } else {
-                        // Token contains id, last4, and card type.
-                        var token = response.id;
-                        // Insert the token into the form so it gets submitted to the server.
-                        $('#stripe_token').val(token);
+  /**
+   * Attaches the commerceStripeForm behavior.
+   *
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   *   Attaches the commerceStripeForm behavior.
+   *
+   * @see Drupal.commerceStripe
+   */
+  Drupal.behaviors.commerceStripeForm = {
+    attach: function (context) {
+      if (typeof drupalSettings.commerceStripe.fetched == 'undefined') {
+        drupalSettings.commerceStripe.fetched = true;
+        // Clear the token every time the payment form is loaded. We only need the token
+        // one time, as it is submitted to Stripe after a card is validated. If this
+        // form reloads it's due to an error; received tokens are stored in the checkout pane.
+        $('#stripe_token').val('');
+        Stripe.setPublishableKey(drupalSettings.commerceStripe.publishableKey);
+        var $form = $('.stripe-form', context).closest('form');
+        var stripeResponseHandler = function (status, response) {
+          if (response.error) {
+            // Show the errors on the form
+            $form.find('.payment-errors').text(response.error.message);
+            $form.find('button').prop('disabled', false);
+          }
+          else {
+            // Token contains id, last4, and card type.
+            var token = response.id;
+            // Insert the token into the form so it gets submitted to the server.
+            $('#stripe_token').val(token);
 
-                        // And re-submit.
-                        $form.get(0).submit();
-                    }
-                };
+            // And re-submit.
+            $form.get(0).submit();
+          }
+        };
 
-                $form.submit(function (e) {
-                    var $form = $(this);
-                    // Disable the submit button to prevent repeated clicks
-                    $form.find('button').prop('disabled', true);
+        $form.submit(function (e) {
+          var $form = $(this);
+          // Disable the submit button to prevent repeated clicks
+          $form.find('button').prop('disabled', true);
 
-                    // Validate card form data.
-                    var card_number = $('.card-number').val();
-                    var card_expiry_month = $('.card-expiry-month').val();
-                    var card_expiry_year = $('.card-expiry-year').val();
-                    var card_cvc = $('.card-cvc').val();
+          // Validate card form data.
+          var card_number = $('.card-number').val();
+          var card_expiry_month = $('.card-expiry-month').val();
+          var card_expiry_year = $('.card-expiry-year').val();
+          var card_cvc = $('.card-cvc').val();
 
-                    var validated = true;
-                    var error_messages = [];
-                    if (!Stripe.card.validateCardNumber(card_number)) {
-                        validated = false;
-                        error_messages.push(Drupal.t('The card number is invalid.'));
-                        $('.card-number').addClass('error');
-                    }
-                    else {
-                        $('.card-number').removeClass('error');
-                    }
-                    if (!Stripe.card.validateExpiry(card_expiry_month, card_expiry_year)) {
-                        validated = false;
-                        error_messages.push(Drupal.t('The expiry date is invalid.'));
-                        $('.card-expiry-month').addClass('error');
-                        $('.card-expiry-year').addClass('error');
-                    }
-                    else {
-                        $('.card-expiry-month').removeClass('error');
-                        $('.card-expiry-year').removeClass('error');
-                    }
-                    if (!Stripe.card.validateCVC(card_cvc)) {
-                        validated = false;
-                        error_messages.push(Drupal.t('The verification code is invalid.'));
-                        $('.card-cvc').addClass('error');
-                    }
-                    else {
-                        $('.card-cvc').removeClass('error');
-                    }
-                    if (error_messages.length > 0) {
-                        var payment_errors = '<ul>';
-                        error_messages.forEach(function(error_message) {
-                            payment_errors += '<li>' + error_message + '</li>';
-                        });
-                        payment_errors += '</ul>';
-                        //$form.find('.payment-errors').html(payment_errors);
-                        $form.find('.payment-errors').html(Drupal.theme('commerceStripeError', payment_errors));
-                    }
+          var validated = true;
+          var error_messages = [];
+          if (!Stripe.card.validateCardNumber(card_number)) {
+            validated = false;
+            error_messages.push(Drupal.t('The card number is invalid.'));
+            $('.card-number').addClass('error');
+          }
+          else {
+            $('.card-number').removeClass('error');
+          }
+          if (!Stripe.card.validateExpiry(card_expiry_month, card_expiry_year)) {
+            validated = false;
+            error_messages.push(Drupal.t('The expiry date is invalid.'));
+            $('.card-expiry-month').addClass('error');
+            $('.card-expiry-year').addClass('error');
+          }
+          else {
+            $('.card-expiry-month').removeClass('error');
+            $('.card-expiry-year').removeClass('error');
+          }
+          if (!Stripe.card.validateCVC(card_cvc)) {
+            validated = false;
+            error_messages.push(Drupal.t('The verification code is invalid.'));
+            $('.card-cvc').addClass('error');
+          }
+          else {
+            $('.card-cvc').removeClass('error');
+          }
+          if (error_messages.length > 0) {
+            var payment_errors = '<ul>';
+            error_messages.forEach(function(error_message) {
+              payment_errors += '<li>' + error_message + '</li>';
+            });
+            payment_errors += '</ul>';
+            $form.find('.payment-errors').html(Drupal.theme('commerceStripeError', payment_errors));
+          }
 
-                    // Create token if the card form was validated.
-                    if (validated) {
-                        Stripe.card.createToken({
-                            number: card_number,
-                            exp_month: card_expiry_month,
-                            exp_year: card_expiry_year,
-                            cvc: card_cvc
-                        }, stripeResponseHandler);
-                    }
+          // Create token if the card form was validated.
+          if (validated) {
+            Stripe.card.createToken({
+              number: card_number,
+              exp_month: card_expiry_month,
+              exp_year: card_expiry_year,
+              cvc: card_cvc
+            }, stripeResponseHandler);
+          }
 
-                    // Prevent the form from submitting with the default action.
-                    if ($('.card-number').length) {
-                        return false;
-                    }
-                });
-            }
-        }
-    };
+          // Prevent the form from submitting with the default action.
+          if ($('.card-number').length) {
+            return false;
+          }
+        });
+      }
+    }
+  };
 
-    $.extend(Drupal.theme, /** @lends Drupal.theme */{
-        commerceStripeError: function (message) {
-            return $('<div class="messages messages--error"></div>').html(message);
-        }
-    });
+  $.extend(Drupal.theme, /** @lends Drupal.theme */{
+    commerceStripeError: function (message) {
+      return $('<div class="messages messages--error"></div>').html(message);
+    }
+  });
 
 })(jQuery, Drupal, drupalSettings);
